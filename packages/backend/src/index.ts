a7932f075c4489672653312ab4304f5718c1e25f
Konrad Gnat <konradmgnat@gmail.com>
2022-01-02 23:52:26 +0100

    add nexus

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,58 +1,29 @@
-console.log("first typescript app Go!")
-
-// node-graphql/src/index.js
-
-// import { ApolloServer } from 'apollo-server'
-// import { typeDefs } from './schema'
-// import { resolvers } from './resolvers'
-
-// const port = process.env.PORT || 9090;
-
-// const server = new ApolloServer({ resolvers, typeDefs });
-
-// server.listen({ port }, () => console.log(`GraphQL server runs at: http://localhost:${port}`));
-
-
 import 'reflect-metadata'
 import { ApolloServer } from 'apollo-server-express'
-import { buildSchema } from 'type-graphql'
-import { PrismaClient } from '@prisma/client';
-// import { resolvers } from "@generated/type-graphql";
-import { resolvers } from "@generated/type-graphql";
+import { createContext } from './graphql/context';
+import { resolvers } from './graphql/resolvers'
+import { schema } from './graphql/schema';
 import Express from 'express'
-import * as tq from 'type-graphql';
 
-// import { MemberResolver } from './resolvers/memberResolver'
-
-const prisma = new PrismaClient()
 
 async function main() {
-  const schema = await tq.buildSchema({
-    emitSchemaFile: true,
-    resolvers,
-})
-
-//   const schema = await buildSchema({
-//     resolvers: [MemberResolver],
-//     emitSchemaFile: true,
-//   })
 
   const app = Express()
 
-  const context = () => {
-    return {
-      prisma
-    }
-  }
-
-  const server = new ApolloServer({
+  // const context = () => {
+  //   return {
+  //     prisma
+  //   }
+  // }
+  const apolloServer = new ApolloServer({
     schema,
-    context,
+    resolvers,
+    context: createContext
   })
 
-  await server.start()
+  await apolloServer.start()
 
-  server.applyMiddleware({ app })
+  apolloServer.applyMiddleware({ app })
 
   app.listen(4000, () =>
     console.log('Server is running on http://localhost:4000/graphql')

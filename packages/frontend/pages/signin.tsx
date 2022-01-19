0803fd1e04d0c24b015fe959e9a7a4ee809741b6
XingyouLiuVic <sos353547464@gmail.com>
2022-01-19 13:07:36 -0600

    add password input

--- packages/frontend/pages/signin.tsx
+++ packages/frontend/pages/signin.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { Container, Flex, Box, Input, Text, Button } from '@chakra-ui/react';
+import { Container, Flex, Box, Input, Text, Button,InputGroup,InputRightElement } from '@chakra-ui/react';
 import { NextPage } from 'next';
 
 const SignIn: NextPage = () => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [show, setShow] = useState(false)
+    const handleClick = () => setShow(!show)
     const signIn = () => {
 
         const auth = getAuth();
@@ -22,13 +24,28 @@ const SignIn: NextPage = () => {
             console.error(errorCode, errorMessage);
           });
     }
+    console.log(show);
     return (
         <Container>
             <Text fontSize='4xl'>Sign In</Text>
             <Flex items="center">
                 <Box width="400px">
                     <Input name="email" value={email} onChange={(e: any) => setEmail(e.target.value)}/>
-                    <Input name="password" value={password} onChange={(e: any) => setPassword(e.target.value)}/>
+                    <InputGroup size='md'>
+                        <Input
+                            pr='4.5rem'
+                            type={show ? 'text' : 'password'}
+                            placeholder='Enter password'
+                            name="password"
+                            value={password}
+                            onChange={(e: any) => setPassword(e.target.value)}
+                        />
+                        <InputRightElement width='4.5rem'>
+                            <Button h='1.75rem' size='sm' onClick={handleClick}>
+                            {show ? 'Hide' : 'Show'}
+                            </Button>
+                        </InputRightElement>
+                    </InputGroup>
                     <Button colorScheme='purple' variant='solid' onClick={signIn}>Sign In</Button>
                 </Box>
             </Flex>

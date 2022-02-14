bf0b143d1c1bdc70dd8861ca1dc016f0e9f1d609
Konrad Gnat <konradmgnat@gmail.com>
2022-02-13 21:26:01 -0700

    deployment

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,10 +1,6 @@
 import type { NextPage } from 'next'
-import Image from 'next/image'
-import { Box, Flex } from '@chakra-ui/react';
 import styles from '../styles/Home.module.css'
 import Hero from '../components/hero';
-import Features from '../components/features';
-import Statistics from '../components/statistics';
 
 const Home: NextPage = () => {
   return (

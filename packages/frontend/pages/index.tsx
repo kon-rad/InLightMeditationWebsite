f57fd62641c22b6ecba9095767d4121b1e93a82c
Konrad Gnat <konradmgnat@gmail.com>
2022-02-15 20:01:34 -0700

    add sign in with email

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,25 +1,76 @@
-import type { NextPage } from 'next'
-import Image from 'next/image'
-import { Box, Flex } from '@chakra-ui/react';
-import styles from '../styles/Home.module.css'
-import Hero from '../components/hero';
-import Features from '../components/features';
-import Statistics from '../components/statistics';
+import type { NextPage } from "next";
+import { useEffect } from "react";
+import Image from "next/image";
+import { Box, Flex } from "@chakra-ui/react";
+import styles from "../styles/Home.module.css";
+import Hero from "../components/hero";
+import Features from "../components/features";
+import Statistics from "../components/statistics";
+import {
+    getAuth,
+    isSignInWithEmailLink,
+    signInWithEmailLink,
+} from "firebase/auth";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Hero />
-      <Flex justifyContent="center" alignItems="center" direction="column">
-        <Box maxWidth="1200px" my={12}>
-          <Features />
-        </Box>
-        <Box maxWidth="1200px" my={12}>
-          <Statistics />
-        </Box>
-      </Flex>
-    </div>
-  )
-}
+    useEffect(() => {
+        checkIfSignedIn();
+    }, []);
 
-export default Home
+    const checkIfSignedIn = () => {
+        // Confirm the link is a sign-in with email link.
+        const auth = getAuth();
+        if (isSignInWithEmailLink(auth, window.location.href)) {
+            // Additional state parameters can also be passed via URL.
+            // This can be used to continue the user's intended action before triggering
+            // the sign-in operation.
+            // Get the email if available. This should be available if the user completes
+            // the flow on the same device where they started it.
+            let email = window.localStorage.getItem("emailForSignIn");
+            if (!email) {
+                // User opened the link on a different device. To prevent session fixation
+                // attacks, ask the user to provide the associated email again. For example:
+                email = window.prompt(
+                    "Please provide your email for confirmation"
+                );
+            }
+            // The client SDK will parse the code from the link for you.
+            signInWithEmailLink(auth, email || "", window.location.href)
+                .then((result) => {
+                    // Clear email from storage.
+                    window.localStorage.removeItem("emailForSignIn");
+                    // You can access the new user via result.user
+                    // Additional user info profile not available via:
+                    // result.additionalUserInfo.profile == null
+                    // You can check if the user is new or existing:
+                    // result.additionalUserInfo.isNewUser
+                    console.log("result: ", result);
+                })
+                .catch((error) => {
+                    // Some error occurred, you can inspect the code: error.code
+                    // Common errors could be invalid email and invalid or expired OTPs.
+                    console.log("error: ", error);
+                });
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <Hero />
+            <Flex
+                justifyContent="center"
+                alignItems="center"
+                direction="column"
+            >
+                <Box maxWidth="1200px" my={12}>
+                    <Features />
+                </Box>
+                <Box maxWidth="1200px" my={12}>
+                    <Statistics />
+                </Box>
+            </Flex>
+        </div>
+    );
+};
+
+export default Home;
--- packages/frontend/pages/signin.tsx
+++ packages/frontend/pages/signin.tsx
@@ -11,20 +11,45 @@ import {
 } from "@chakra-ui/react";
 import { NextPage } from "next";
 import { useAuth } from "../context/auth";
-import Router from 'next/router'
+import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
+import Router from "next/router";
 import { toast } from "react-toastify";
+
+const actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be in the authorized domains list in the Firebase Console.
+    url: "http://localhost:3004",
+    // This must be true.
+    handleCodeInApp: true,
+    // dynamicLinkDomain: "https://inlight.signin.link",
+};
 
 const SignIn: NextPage = () => {
     const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const [show, setShow] = useState(false);
-    const handleClick = () => setShow(!show);
-    const { login } = useAuth();
 
     const onLogIn = async () => {
+        const auth = getAuth();
+
         try {
-            await login(email, password);
-            Router.push("/");
+            sendSignInLinkToEmail(auth, email, actionCodeSettings)
+                .then(() => {
+                    window.localStorage.setItem("emailForSignIn", email);
+                    Router.push("/");
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    toast.error(`${errorCode}: ${errorMessage}`, {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                    });
+                    return;
+                });
         } catch(e: any) {
             toast.error(`${e}`, {
                 position: "top-right",
@@ -49,21 +74,6 @@ const SignIn: NextPage = () => {
                         value={email}
                         onChange={(e: any) => setEmail(e.target.value)}
                     />
-                    <InputGroup size="md" my={4}>
-                        <Input
-                            pr="4.5rem"
-                            type={show ? "text" : "password"}
-                            placeholder="password"
-                            name="password"
-                            value={password}
-                            onChange={(e: any) => setPassword(e.target.value)}
-                        />
-                        <InputRightElement width="4.5rem">
-                            <Button h="1.75rem" size="sm" onClick={handleClick}>
-                                {show ? "Hide" : "Show"}
-                            </Button>
-                        </InputRightElement>
-                    </InputGroup>
                 </Box>
                 <Button colorScheme="purple" variant="solid" onClick={onLogIn}>
                     Sign In

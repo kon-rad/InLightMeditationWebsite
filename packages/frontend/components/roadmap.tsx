f024097d519a8097b78597104c83efe17db44d8d
Konrad Gnat <konradmgnat@gmail.com>
2022-03-01 18:30:06 -0800

    add roadmap component ;

---
+++ packages/frontend/components/roadmap.tsx
@@ -0,0 +1,11 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
+
+const Roadmap = () => {
+    return (
+        <Box>
+            <Text fontSize="4xl">Roadmap</Text> 
+        </Box>
+    )
+}
+
+export default Roadmap;
--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 import Hero from '../components/hero';
 import Features from '../components/features';
+import Roadmap from '../components/roadmap';
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Hero />
       <Features />
+      <Roadmap />
     </div>
   )
 }
